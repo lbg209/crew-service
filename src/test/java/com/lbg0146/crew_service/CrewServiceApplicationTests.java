128a246e88861aa4f34c65936865270a86fab8fb
LEE <lbg0146@gmail.com>
2026-06-20 03:51:19 +0900

    first commit

---
+++ src/test/java/com/lbg0146/crew_service/CrewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbg0146.crew_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
