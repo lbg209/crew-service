b5b077d963df7212baafb8f81976412652bb00f6
LEE <lbg0146@gmail.com>
2026-07-15 23:55:28 +0900

    feat: Docker 배포 환경 설정 추가

---
+++ src/main/java/com/lbg0146/crew_service/exception/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package com.lbg0146.crew_service.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class InvalidTokenException extends AuthenticationException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
